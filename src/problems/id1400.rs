5631f3e4d405bbb9b411651443817f5e47d6b993
Vanta_1 <mcoopersandys@gmail.com>
2025-01-11 01:21:25 -0500

    nvm i solved it just now. had to use a hint and it spoiled it though

--- src/problems/id1400.rs
+++ src/problems/id1400.rs
@@ -6,8 +6,21 @@
 pub struct Solution;
 
 impl Solution {
+    /// Used hints to help solve, first submission due to issue with test case 93, or the 4th test in run().
+    /// Second submission passed with a runtime of 0ms.
     pub fn can_construct(s: String, k: i32) -> bool {
-        todo!()
+        if s.len() < k as usize {
+            return false;
+        }
+        let mut counts = [0u32; 26];
+        for c in s.chars() {
+            counts[((c as u8) - 97) as usize] += 1;
+        }
+        let odds: Vec<u32> = counts.into_iter().filter(|n| n % 2 != 0).collect();
+        if odds.len() > k as usize {
+            return false;
+        }
+        true
     }
 }
 
@@ -16,4 +29,5 @@ pub fn run() {
     assert!(Solution::can_construct("annabelle".to_string(), 2));
     assert!(!Solution::can_construct("leetcode".to_string(), 3));
     assert!(Solution::can_construct("true".to_string(), 4));
+    assert!(!Solution::can_construct("cr".to_string(), 7));
 }

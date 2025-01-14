8e04e9ae15776e7f3698e0ed3f7d7bcc021d1bc6
Vanta_1 <mcoopersandys@gmail.com>
2025-01-13 22:30:46 -0500

    havent solved 2116 yet, feel like im close though

--- src/problems/id2116.rs
+++ src/problems/id2116.rs
@@ -21,8 +21,54 @@ impl Solution {
     // some notes on the problem: i could probably just count the number of '('s or ')'s
     // and make sure they can be made the same
     // ig by tracking the number of each that can be changed
+    // '(' = open = op, ')' = close = cl
     pub fn can_be_valid(s: String, locked: String) -> bool {
-        todo!()
+        if s.len() % 2 != 0 {
+            // odd length means it's invalid automatically
+            return false;
+        }
+        let mut c_op: i32 = 0;
+        let mut c_cl: i32 = 0;
+        let mut op_unlocked: i32 = 0;
+        let mut cl_unlocked: i32 = 0;
+
+        let s: Vec<char> = s.chars().collect();
+        let locked: Vec<char> = locked.chars().collect();
+
+        s.into_iter()
+            .enumerate()
+            .for_each(|(i, paren)| match paren {
+                '(' => {
+                    c_op += 1;
+                    if locked[i] == '0' {
+                        op_unlocked += 1;
+                    }
+                }
+                _ => {
+                    c_cl += 1;
+                    if locked[i] == '0' {
+                        cl_unlocked += 1;
+                    }
+                }
+            });
+
+        if c_op == c_cl {
+            return true;
+        }
+
+        if c_op > c_cl {
+            if (c_op - c_cl) >= (((op_unlocked - cl_unlocked) / 2) as f32).ceil() as i32
+                && ((op_unlocked - cl_unlocked) / 2) != 0
+            {
+                return true;
+            }
+        } else if (c_cl - c_op) >= (((cl_unlocked - op_unlocked) / 2) as f32).ceil() as i32
+            && ((cl_unlocked - op_unlocked) / 2) != 0
+        {
+            return true;
+        }
+
+        false
     }
 }
 
@@ -37,4 +83,8 @@ pub fn run() {
         "0000".to_string()
     ));
     assert!(!Solution::can_be_valid(")".to_string(), "0".to_string()));
+    assert!(!Solution::can_be_valid(
+        "))))(())((()))))((()((((((())())((()))((((())()()))(()".to_string(),
+        "101100101111110000000101000101001010110001110000000101".to_string()
+    ))
 }
